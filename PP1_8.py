e3aae0f82b674844d335198996af6656c7ffe45e
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-10-01 13:45:05 +0000

    gh

--- PP1_8.py
+++ PP1_8.py
@@ -7,33 +7,35 @@ def q1():
   print(bool1 or bool2)
 def q2():
   #Write Assignment code here
-  number=eval(input("Enter an integer"))
+  number=eval(input("Enter an integer: "))
   bool1=not number==0
   print(bool1)
 def q3():
   #Write Assignment code here
-  number=eval(input("Enter a number"))
-  print(number > 0 and number < 10) 
+  number=eval(input("Enter a number: "))
+  print(number >= 0 and number <= 10)
 def q4():
   #Write Assignment code here
-  food=input("Enter a food, with the first letter being capitalized")
-  drink=input("Enter a drink, with the first letter being capitalized")
-  if food=='Pizza' and drink=='Pop':
+  food=input("Input food: ")
+  drink=input("Input drink: ")
+  if food=='pizza' and drink=='pop':
+    (False)
     print(False)
   else:
+    (True)
     print(True)
 def q5():
   #Write Assignment code here
-  number=eval(input("Enter an integer"))
+  number=eval(input("Enter an integer: "))
   if number%2==0:
-    print(True)
+    print(f'The integer {number} is True')
   else:
-    print(False)
+    print(f'The integer {number} is False')
 #Do not edit code after this
 #Comment out the followwing code when running tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
